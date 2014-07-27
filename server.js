50bf83e570e52a8ce678a78a6ea59ad1844a7bb2
Martin Naumann <mr.avgp@gmail.com>
2014-07-27 17:50:17 -0400

    Using docker-exec to execute the analysis

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express     = require('express'),
-    bodyparser  = require('body-parser'),
-    urlParser   = require('url'),
-    exec        = require('child_process').exec;
+var express      = require('express'),
+    bodyparser   = require('body-parser'),
+    urlParser    = require('url'),
+    exec         = require('child_process').exec,
+    DockerRunner = require('docker-exec');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -10,14 +11,23 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.post("/analyse", function(req, res) { 
     var path     = urlParser.parse(req.param('url')).pathname,
         parts    = path.split('/'),
+        userName = parts[parts.length - 2],
         repoName = parts[parts.length - 1];
         
     if(repoName.indexOf('.') > 0) repoName = repoName.slice(0, repoName.lastIndexOf('.'));
 
-    console.log('cd /tmp && git clone ' + req.param('url') + ' && cd /tmp/' + repoName + ' && cloc --report-file ' + __dirname + '/public/repos/' + repoName + '.cloc --csv --by-file .');
-    exec('cd /tmp && git clone ' + req.param('url') + ' && cd /tmp/' + repoName + ' && cloc --report-file data.cloc --csv --by-file . && cloc2json data.cloc > ' + __dirname + '/public/repos/' + repoName + '.json && rm -rf /tmp/' + repoName);
-
-    res.send(repoName); 
+    var ds = new DockerRunner();
+    ds.start({Image: "codescape-worker", Volumes: { "/repo": {}}, Binds: [ __dirname + "/public/repos:/repo:rw" ]}).then(function (stream) {
+	stream.pipe(process.stdout);
+	var cloneDir = userName + "_" + repoName;
+	console.log("Cloning into ", cloneDir);
+        return ds.run("git clone " + req.param("url") + " " + cloneDir + " && cd /tmp/" + cloneDir + " && cloc --report-file report.cloc --csv --by-file . && cloc2json report.cloc > /repo/" + cloneDir + ".json");
+    }).then(function() {
+	console.log("Stopping container..");
+        return ds.stop();
+    }).then(function() {
+        res.send("Done");
+    }).catch(function(e) { console.log("ERROR:", e); });
 });
 
 
